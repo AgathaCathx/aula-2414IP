10dffc8a87d0a5047405b5ff1da5b704e4d28080
AgathaCathx <agathacatherineq@hotmail.com>
2025-04-02 15:51:24 -0300

    24.py

---
+++ 24.py
@@ -0,0 +1,33 @@
+#contador = 0
+#while contador < 5:
+#    print(f"contagem: {contador}")
+#    contador += 1 
+
+#senha = "1234"
+#tentativa = ""
+#while tentativa != senha:
+#    tentativa = input("digite a senha:")
+#print("Acesso concedido!")
+
+#while True:
+#    comando= input("digite 'sair' para parar:")
+#    if comando.lower() == "sair":
+#        print("ate a proxima")
+#        break
+#    print(f"voce digitou: {comando}")
+
+#calculadora simples usando while
+while True:
+    operacao = input("escolha a operaçao: (+,-,*,/)")
+    num1 = float(input("escreva um numero:"))
+    num2 = float(input("digite o outro numero"))
+    if operacao =='+':
+         print("num1+num2")
+         pass
+    elif operacao == '-':
+         print("num1-num2")
+         pass
+    elif operacao == '*':
+         print("num1*num2")
+    elif operacao == '/':
+         print("num1/num2")
--- observaçoes.py
+++ observaçoes.py
@@ -7,3 +7,12 @@
 #input = pedir algo;
 #print = pergunta;
 #f' = formatar o print.
+#while= enquanto
+#for= para
+#true = verdade
+#false+ falso
+#break= quebrado
+#lower= diminuitivo
+#uppr= almentativo
+# != =perguntar se e diferente
+# == Significa igual
